102cff75a40018a5752d858b095629fc91d25d34
김봉연 <bykim@angeluce.kr>
2022-07-25 15:51:53 +0900

    Initial commit

---
+++ src/main/java/com/example/ec2spring/Ec2springApplication.java
@@ -0,0 +1,14 @@
+package com.example.ec2spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2springApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ec2springApplication.class, args);
+        System.out.println("hello wordl");
+    }
+
+}
---
+++ src/test/java/com/example/ec2spring/Ec2springApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ec2spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2springApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
