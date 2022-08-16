d21157bc1232477be7c4871c2390f02d161287fa
김봉연 <bykim@angeluce.kr>
2022-08-17 01:34:46 +0900

    이미지 업로드 테스트2

--- src/main/java/com/example/ec2spring/quillEditor/QuillEditorService.java
+++ src/main/java/com/example/ec2spring/quillEditor/QuillEditorService.java
@@ -45,7 +45,7 @@ public class QuillEditorService {
                 prdSrcImg = dto.getHtmlTxt().split(",")[1];
                 // 파일을 출력, 저장한다.
                 try {
-                    fileName = "test.png";
+                    fileName = "test";
 //                    dto.setBizrnoEvdnc(fileName);
 
                     Set<PosixFilePermission> perms = new HashSet<>();
